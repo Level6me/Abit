b2a7235a6611efae42c7c4cc3c6ab257e3feaf99
Level6me <i@level6.me>
2026-09-06 17:24:27 +0800

    chore(build): 更新构建时间戳与打包产物

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Provides instant shell caching, Stale-While-Revalidate loading and enables PWA installation.
  */
 
-const CACHE_NAME = 'abit-pwa-v2026.09.06-1723';
+const CACHE_NAME = 'abit-pwa-v2026.09.06-1724';
 const STATIC_ASSETS = [
     './',
     './index.html',
--- src/js/constants.js
+++ src/js/constants.js
@@ -4,8 +4,8 @@
  */
 
 // Application Metadata (Automatically updated during build)
-const APP_VERSION = 'v2026.09.06-1723';
-const APP_BUILD_TIME = '2026-09-06 17:23:48';
+const APP_VERSION = 'v2026.09.06-1724';
+const APP_BUILD_TIME = '2026-09-06 17:24:19';
 const APP_REPO_URL = 'https://github.com/Level6me/Abit';
 
 // Popular Preset Search Plugins Repository (100% Verified Working URLs)
