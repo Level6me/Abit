59e91c9ebf011ae939b0fcb3d99f33cb80a5d33c
Level6me <i@level6.me>
2026-09-06 17:46:21 +0800

    chore(build): sync final build artifacts

--- src/js/constants.js
+++ src/js/constants.js
@@ -5,7 +5,7 @@
 
 // Application Metadata (Automatically updated during build)
 const APP_VERSION = 'v2026.09.06-1746';
-const APP_BUILD_TIME = '2026-09-06 17:46:07';
+const APP_BUILD_TIME = '2026-09-06 17:46:16';
 const APP_REPO_URL = 'https://github.com/Level6me/Abit';
 
 // Popular Preset Search Plugins Repository (100% Verified Working URLs)
